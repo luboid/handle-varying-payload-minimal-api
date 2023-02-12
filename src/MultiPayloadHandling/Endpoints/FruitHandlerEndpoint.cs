99454b5b423a2d2c8bd754eb49a39cd38607bcac
Lyubomir Lyubenov <luboid@yahoo.com>
2023-02-12 16:49:35 +0200

    simple get endpoint

--- src/MultiPayloadHandling/Endpoints/FruitHandlerEndpoint.cs
+++ src/MultiPayloadHandling/Endpoints/FruitHandlerEndpoint.cs
@@ -7,18 +7,30 @@ public static class FruitHandlerEndpoint
     public static void AddFruitHandlerEndpoint(this RouteGroupBuilder group)
     {
         group.MapPost("/FruitHandler", FruitHandler)
-        .WithName("FruitHandler")
+        .WithName("FruitHandlerCreate")
         .WithOpenApi()
         .Accepts<Fruit>("application/json");
+        group.MapGet("/FruitHandler", FruitHandlerGet)
+        .WithName("FruitHandlerGet")
+        .WithOpenApi();
     }
 
     public static async Task<IResult> FruitHandler(
-        [FromBody, Validate] Fruit fruit, 
+        [FromBody, Validate] Fruit fruit,
         [FromServices] IFruitHandlerFactory fruitHandlerFactory,
         CancellationToken cancellationToken)
     {
         var handler = fruitHandlerFactory.Create(fruit.Type);
         var juiceDesc = await handler.HandleAsync(fruit, cancellationToken);
         return Results.Ok(new { Juice = juiceDesc });
+    }
+
+    public static Task<IResult> FruitHandlerGet(CancellationToken cancellationToken)
+    {
+        return Task.FromResult(Results.Ok(new Fruit[] 
+        { 
+            new Apple { AppleName = "The Apple" }, 
+            new Pear { PearName = "The Pear" } 
+        }));
     }
 }
